da94cc8fa7476ff4ab14b8bb7e66f362f5b97159
MichaelAuw <108010620+MichaelAuw@users.noreply.github.com>
2023-01-14 17:18:13 +0700

    Add files via upload

---
+++ Sessions/12/English/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+//connection to website
+
+$servername = "localhost";
+$username = "";
+$password = "";
+$db_name = "ruangsiswa";
+
+$conn = new mysqli($servername,$username,$password,$db_name);
+
+?>
